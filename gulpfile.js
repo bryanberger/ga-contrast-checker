306be421d671187937e0e9365a5a8e68a84dbd02
Bryan Berger <bb@ga.co>
2016-10-27 16:57:43 -0400

    added contrast2.html + all colors

--- gulpfile.js
+++ gulpfile.js
@@ -22,6 +22,7 @@
       fs.writeFileSync('contrast.json', JSON.stringify(result, null, 2));
       fs.writeFileSync('contrast.csv', getCSV(result));
       fs.writeFileSync('contrast.html', getHTML(result));
+      fs.writeFileSync('contrast2.html', getComplexHTML(result));
 
   });
 
@@ -64,6 +65,41 @@ function getHTML(result) {
         }
 
         str += '</div>\r\n';
+    }
+
+    return str;
+}
+
+//Convert the Colorable JSON object to a hacky complex HTML page
+function getComplexHTML(result) {
+    var str = '<style>*{margin:0;padding:0;box-sizing:border-box;font-family:"Circular Std"</style>\r\n';
+        str += '<h4 style="padding:1em;text-align:center;">Color combinations that meet the AA 4.5:1 contrast ratio</h4>\r\n';
+
+    for (var i = 0; i < result.length; i++) {
+      var color = result[i];
+      var combos = result[i].combinations;
+
+      str += '<div style="overflow:hidden; clear:left; margin-bottom:2em; ">';
+      str += '<div style="margin-left:1%; font-family:monospace; color: #666;">colors that pass on '+color.hex + '(' + color.name+')</div>';
+
+      for (var x = 0; x < combos.length; x++) {
+        var combo = combos[x];
+        var dict = combo.accessibility;
+
+        if(dict['aa']) {
+          var bodyColor = combo.hex;
+
+          str += '<div style="width:23%; margin: .5em 1%; background-color:'+color.hex+'; position:relative; padding:1em; float:left; overflow:hidden;">';
+          str += '<div style="color:'+bodyColor+'; font-size:23px; font-weight:bold; margin-bottom:8px">Flatiron</div>';
+          str += '<div style="margin-bottom:8px; color:'+combo.hex+'">GA New York City (East)</div>';
+          str += '<div style="margin-bottom:8px; color:'+bodyColor+'">902 Broadway, Floor 4<br>New York, NY 10010</div>';
+          str += '<div style="color:'+bodyColor+';">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce <a style="color:'+combo.hex+'" href="#">faucibus exinect</a> tincidunt. Aenean venenatis consectetur augue, eget blandit</div>';
+          str += '<div style="margin-top:0.5em; color:'+bodyColor+'">'+combo.contrast.toFixed(2)+'</div>'
+          str += '</div>';
+        }
+      }
+
+      str += '</div>\r\n';
     }
 
     return str;
